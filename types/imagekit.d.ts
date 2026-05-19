16bb91686d8d3db0852aff63bb83c1be4f0b986e
dikie001 <omondidickens255@gmail.com>
2026-05-19 11:16:47 +0300

    feat: update ImageKit module declaration to use correct import path and improve type definitions

--- types/imagekit.d.ts
+++ types/imagekit.d.ts
@@ -1,14 +1,14 @@
-declare module '@imagekit/javascript' {
+declare module "@imagekit/next" {
   export default class ImageKit {
-    constructor(config: { publicKey?: string; privateKey?: string; urlEndpoint?: string })
-    upload(options: { file: string; fileName?: string; folder?: string }): Promise<any>
-  }
-}
+    constructor(config: {
+      publicKey?: string
+      privateKey?: string
+      urlEndpoint?: string
+    })
+    upload(options: {
       file: string
       fileName?: string
       folder?: string
     }): Promise<any>
   }
-  const _default: any
-  export default _default
 }
